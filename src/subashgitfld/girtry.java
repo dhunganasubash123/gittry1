288168932416a8aa03013a0bc643c91bc615b28b
subashdhungana123 <92055996+subashdhungana123@users.noreply.github.com>
2021-11-16 13:28:52 -0600

    just check

--- src/subashgitfld/girtry.java
+++ src/subashgitfld/girtry.java
@@ -5,7 +5,7 @@ public class girtry {
 	public static void main(String[] args) {
 System.out.println("hi guys");
 
-System.out.println("Hi Supriya this is Subhash");
+System.out.println("Hi Supriya this is Subhash !");
 
 
 		
